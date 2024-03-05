7eec70b6384cf1724b28e4a995f740f1602a7f23
Bartlomiej Bielecki <b.bielecki@micro-solutions.pl>
2024-03-05 11:03:43 +0100

    Bootstrapper

---
+++ src/Bootstrapper/DeepFocus.Bootstrapper/Program.cs
@@ -0,0 +1,15 @@
+using Modular.Infrastructure.Modules;
+
+namespace DeepFocus.Bootstrapper
+{
+    public class Program
+    {
+        public static Task Main(string[] args)
+            => CreateHostBuilder(args).Build().RunAsync();
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
+                .ConfigureModules();
+    }
+}
---
+++ src/Bootstrapper/DeepFocus.Bootstrapper/Startup.cs
@@ -0,0 +1,53 @@
+﻿using Modular.Abstractions.Modules;
+using Modular.Infrastructure;
+using Modular.Infrastructure.Modules;
+using System.Reflection;
+
+namespace DeepFocus.Bootstrapper
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IList<Assembly> _assemblies;
+        private readonly IList<IModule> _modules;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _assemblies = ModuleLoader.LoadAssemblies(configuration, "DeepFocus.Modules.");
+            _modules = ModuleLoader.LoadModules(_assemblies);
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddModularInfrastructure(_configuration, _assemblies, _modules);
+            foreach (var module in _modules)
+            {
+                module.Register(services);
+            }
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
+        {
+            logger.LogInformation($"Modules: {string.Join(", ", _modules.Select(x => x.Name))}");
+            app.UseModularInfrastructure();
+            foreach (var module in _modules)
+            {
+                module.Use(app);
+            }
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapGet("/", context => context.Response.WriteAsync("DeepFocus API!"));
+                endpoints.MapModuleInfo();
+            });
+
+            //app.UseConvey();
+            //app.UseRabbitMq();
+
+            _assemblies.Clear();
+            _modules.Clear();
+        }
+    }
+}
