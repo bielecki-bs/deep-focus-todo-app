e3e9d762d8efc228aa815ebda08b08e9ccdd8de0
bielecki.bs <bielecki.bs@gmail.com>
2024-03-07 22:00:21 +0100

    User module init

---
+++ src/Modules/Users/DeepFocus.Modules.Users.Api/UsersModule.cs
@@ -0,0 +1,30 @@
+﻿using DeepFocus.Modules.Users.Core;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Modular.Abstractions.Modules;
+
+
+namespace DeepFocus.Modules.Users.Api
+{
+    internal class UsersModule : IModule
+    {
+        public const string BasePath = "users-module";
+        public string Name { get; } = "Users";
+        public string Path => BasePath;
+
+        public IEnumerable<string> Policies { get; } = new[]
+        {
+         "users"
+        };
+
+        public void Register(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddCore(configuration);
+        }
+
+        public void Use(IApplicationBuilder app)
+        {
+        }
+    }
+}
---
+++ src/Modules/Users/DeepFocus.Modules.Users.Core/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using DeepFocus.Modules.Users.Core.DAL;
+using DeepFocus.Modules.Users.Core.DAL.Repositories;
+using DeepFocus.Modules.Users.Core.Entities;
+using DeepFocus.Modules.Users.Core.Repositories;
+using DeepFocus.Modules.Users.Core.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Modular.Infrastructure.Postgres;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("DeepFocus.Modules.Users.Api")]
+namespace DeepFocus.Modules.Users.Core
+{
+    internal static class Extensions
+    {
+        public static IServiceCollection AddCore(this IServiceCollection services, IConfiguration configuration)
+            => services
+                .AddScoped<IUserRepository, UserRepository>()
+                .AddSingleton<IPasswordHasher<User>, PasswordHasher<User>>()
+                .AddTransient<IIdentityService, IdentityService>()
+                .AddPostgres<UsersDbContext>(configuration);
+    }
+}
